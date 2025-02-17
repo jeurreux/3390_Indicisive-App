ecd8565f9daeadb449eda632582ddc93a53c6129
jeurreux <it7sme@gmail.com>
2025-02-17 11:08:15 -0800

    Initial Commit

---
+++ Indicisive App/Indicisive_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indicisive_AppApp.swift
+//  Indicisive App
+//
+//  Created by Joshua on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Indicisive_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Indicisive AppTests/Indicisive_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Indicisive_AppTests.swift
+//  Indicisive AppTests
+//
+//  Created by Joshua on 2/17/25.
+//
+
+import Testing
+@testable import Indicisive_App
+
+struct Indicisive_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
